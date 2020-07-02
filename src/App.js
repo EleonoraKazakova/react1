05a517471ff8cf143c5b54c901982aa059bd2315
Eleonora <abc-1313@yandex.ru>
2020-07-02 19:08:04 +0200

    remake Menu

--- src/App.js
+++ src/App.js
@@ -8,48 +8,48 @@ import Text from "./Text";
 import Search from "./Search";
 import Cat from "./Cat";
 import AlbumSection from './AlbumSection'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [page, setPage] = useState("1");
 
   return (
-    <div className="App">
-      <Menu />
+    <Router>
+      <div className="App">
+        <Menu />
+        <Switch>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/albums'>
+            <AlbumSection />
+          </Route>
+          <Route path='/to-do'>
+            <div className='App-todo'>
+              <Cat />
+              <div>
+                <button className="block" onClick={() => setPage("1")}>
+                  List
+                </button>
+                <button className="block" onClick={() => setPage("2")}>
+                  Empty
+                </button>
+              </div>
 
-      <Gallery />
+              <h1>The new list</h1>
+              <br />
+              {page === "1" ? <List /> : <Empty />}
+            </div>
+          </Route>
+          <Route path='/'>
 
-      <header className="App-header">
-        <Text />
-        <Search />
-        <Cat />
-        <AlbumSection />
-
-        <div>
-          <button className="block" onClick={() => setPage("1")}>
-            List
-          </button>
-          <button className="block" onClick={() => setPage("2")}>
-            Empty
-          </button>
-        </div>
-
-        <h1>The new list</h1>
-        <br />
-        {page === "1" ? <List /> : <Empty />}
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Text {abc(2) * 3}
-        </a>
-      </header>
-    </div>
+            <Gallery />
+            <Text />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-function abc(a) {
-  return a + 10;
-}
+
 export default App;
